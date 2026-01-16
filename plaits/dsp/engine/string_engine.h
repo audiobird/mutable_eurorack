79b38703b59c37eefbae2d92072f37808e4515c7
audiobird <cantrellhepner@gmail.com>
2026-01-16 11:23:04 -0800

    format

--- plaits/dsp/engine/string_engine.h
+++ plaits/dsp/engine/string_engine.h
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-// 
+//
 // See http://creativecommons.org/licenses/MIT/ for more information.
 //
 // -----------------------------------------------------------------------------
@@ -34,33 +34,28 @@
 
 namespace plaits {
 
-const int kNumStrings = 3;
-
 class StringEngine : public Engine {
- public:
-  StringEngine() { }
-  ~StringEngine() { }
-  
-  virtual void Init(stmlib::BufferAllocator* allocator);
+public:
+  StringEngine() {}
+  ~StringEngine() {}
+
+  virtual void Init(stmlib::BufferAllocator *allocator);
   virtual void Reset();
-  virtual void LoadUserData(const uint8_t* user_data) { }
-  virtual void Render(const EngineParameters& parameters,
-      float* out,
-      float* aux,
-      size_t size,
-      bool* already_enveloped);
+  virtual void LoadUserData(const uint8_t *user_data) {}
+  virtual void Render(const EngineParameters &parameters, float *out,
+                      float *aux, size_t size, bool *already_enveloped);
 
- private:
-  StringVoice voice_[kNumStrings];
+private:
+  StringVoice voice_;
 
-  float f0_[kNumStrings];
+  float f0_;
   DelayLine<float, 16> f0_delay_;
   int active_string_;
-  float* temp_buffer_;
-  
+  float *temp_buffer_;
+
   DISALLOW_COPY_AND_ASSIGN(StringEngine);
 };
 
-}  // namespace plaits
+} // namespace plaits
 
-#endif  // PLAITS_DSP_ENGINE_STRING_ENGINE_H_
+#endif // PLAITS_DSP_ENGINE_STRING_ENGINE_H_
