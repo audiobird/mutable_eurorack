18dcb4132c2176ca09a21032b84cd7b5f21f5485
audiobird <cantrellhepner@gmail.com>
2026-01-20 11:30:53 -0800

    .

--- plaits/dsp/engine/grain_engine.h
+++ plaits/dsp/engine/grain_engine.h
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-// 
+//
 // See http://creativecommons.org/licenses/MIT/ for more information.
 //
 // -----------------------------------------------------------------------------
@@ -37,32 +37,29 @@
 #include "plaits/dsp/oscillator/z_oscillator.h"
 
 namespace plaits {
-  
+
 class GrainEngine : public Engine {
- public:
-  GrainEngine() { }
-  ~GrainEngine() { }
-  
-  virtual void Init(stmlib::BufferAllocator* allocator);
+public:
+  GrainEngine() {}
+  ~GrainEngine() {}
+
+  virtual void Init(stmlib::BufferAllocator *allocator);
   virtual void Reset();
-  virtual void LoadUserData(const uint8_t* user_data) { }
-  virtual void Render(const EngineParameters& parameters,
-      float* out,
-      float* aux,
-      size_t size,
-      bool* already_enveloped);
-    
- private:
+  virtual void LoadUserData(const uint8_t *user_data) {}
+  virtual void Render(const EngineParameters &parameters, float *out,
+                      float *aux, size_t size, bool *already_enveloped);
+
+private:
   GrainletOscillator grainlet_[2];
   // VOSIMOscillator vosim_oscillator_;
   ZOscillator z_oscillator_;
   stmlib::OnePole dc_blocker_[2];
-  
+
   float grain_balance_;
-  
+
   DISALLOW_COPY_AND_ASSIGN(GrainEngine);
 };
 
-}  // namespace plaits
+} // namespace plaits
 
-#endif  // PLAITS_DSP_ENGINE_GRAIN_ENGINE_H_
+#endif // PLAITS_DSP_ENGINE_GRAIN_ENGINE_H_
