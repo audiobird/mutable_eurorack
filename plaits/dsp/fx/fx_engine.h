b045eafac017131e6802da2767bae2077d31dac9
audiobird <cantrellhepner@gmail.com>
2026-01-26 15:40:32 -0800

    format

--- plaits/dsp/fx/fx_engine.h
+++ plaits/dsp/fx/fx_engine.h
@@ -8,10 +8,10 @@
 // to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
-// 
+//
 // The above copyright notice and this permission notice shall be included in
 // all copies or substantial portions of the Software.
-// 
+//
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
 // FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -19,7 +19,7 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 // THE SOFTWARE.
-// 
+//
 // See http://creativecommons.org/licenses/MIT/ for more information.
 //
 // -----------------------------------------------------------------------------
@@ -33,141 +33,111 @@
 
 #include "stmlib/stmlib.h"
 
-#include "stmlib/dsp/dsp.h"
 #include "stmlib/dsp/cosine_oscillator.h"
+#include "stmlib/dsp/dsp.h"
 
 namespace plaits {
 
 #define TAIL , -1
 
-enum Format {
-  FORMAT_12_BIT,
-  FORMAT_16_BIT,
-  FORMAT_32_BIT
-};
+enum Format { FORMAT_12_BIT, FORMAT_16_BIT, FORMAT_32_BIT };
 
-enum LFOIndex {
-  LFO_1,
-  LFO_2
-};
+enum LFOIndex { LFO_1, LFO_2 };
 
-template<Format format>
-struct DataType { };
+template <Format format> struct DataType {};
 
-template<>
-struct DataType<FORMAT_12_BIT> {
+template <> struct DataType<FORMAT_12_BIT> {
   typedef uint16_t T;
-  
+
   static inline float Decompress(T value) {
     return static_cast<float>(static_cast<int16_t>(value)) / 4096.0f;
   }
-  
+
   static inline T Compress(float value) {
     return static_cast<uint16_t>(
         stmlib::Clip16(static_cast<int32_t>(value * 4096.0f)));
   }
 };
 
-template<>
-struct DataType<FORMAT_16_BIT> {
+template <> struct DataType<FORMAT_16_BIT> {
   typedef uint16_t T;
-  
+
   static inline float Decompress(T value) {
     return static_cast<float>(static_cast<int16_t>(value)) / 32768.0f;
   }
-  
+
   static inline T Compress(float value) {
     return static_cast<uint16_t>(
         stmlib::Clip16(static_cast<int32_t>(value * 32768.0f)));
   }
 };
 
-template<>
-struct DataType<FORMAT_32_BIT> {
+template <> struct DataType<FORMAT_32_BIT> {
   typedef float T;
-  
+
   static inline float Decompress(T value) {
-    return value;;
-  }
-  
-  static inline T Compress(float value) {
     return value;
+    ;
   }
+
+  static inline T Compress(float value) { return value; }
 };
 
-template<
-    size_t size,
-    Format format = FORMAT_12_BIT>
-class FxEngine {
- public:
+template <size_t size, Format format = FORMAT_12_BIT> class FxEngine {
+public:
   typedef typename DataType<format>::T T;
-  FxEngine() { }
-  ~FxEngine() { }
+  FxEngine() {}
+  ~FxEngine() {}
 
-  void Init(T* buffer) {
-    buffer_ = buffer;
-  }
-  
+  void Init(T *buffer) { buffer_ = buffer; }
+
   void Clear() {
     std::fill(&buffer_[0], &buffer_[size], 0);
     write_ptr_ = 0;
   }
 
-  struct Empty { };
-  
-  template<int32_t l, typename T = Empty>
-  struct Reserve {
+  struct Empty {};
+
+  template <int32_t l, typename T = Empty> struct Reserve {
     typedef T Tail;
-    enum {
-      length = l
-    };
+    enum { length = l };
   };
-  
-  template<typename Memory, int32_t index>
-  struct DelayLine {
+
+  template <typename Memory, int32_t index> struct DelayLine {
     enum {
       length = DelayLine<typename Memory::Tail, index - 1>::length,
-      base = DelayLine<Memory, index - 1>::base + DelayLine<Memory, index - 1>::length + 1
+      base = DelayLine<Memory, index - 1>::base +
+             DelayLine<Memory, index - 1>::length + 1
     };
   };
 
-  template<typename Memory>
-  struct DelayLine<Memory, 0> {
-    enum {
-      length = Memory::length,
-      base = 0
-    };
+  template <typename Memory> struct DelayLine<Memory, 0> {
+    enum { length = Memory::length, base = 0 };
   };
 
   class Context {
-   friend class FxEngine;
-   public:
-    Context() { }
-    ~Context() { }
-    
-    inline void Load(float value) {
-      accumulator_ = value;
-    }
+    friend class FxEngine;
+
+  public:
+    Context() {}
+    ~Context() {}
+
+    inline void Load(float value) { accumulator_ = value; }
 
     inline void Read(float value, float scale) {
       accumulator_ += value * scale;
     }
 
-    inline void Read(float value) {
-      accumulator_ += value;
-    }
+    inline void Read(float value) { accumulator_ += value; }
 
-    inline void Write(float& value) {
-      value = accumulator_;
-    }
+    inline void Write(float &value) { value = accumulator_; }
 
-    inline void Write(float& value, float scale) {
+    inline void Write(float &value, float scale) {
       value = accumulator_;
       accumulator_ *= scale;
     }
-    
-    template<typename D>
-    inline void Write(D& d, int32_t offset, float scale) {
+
+    template <typename D> inline void Write(D &d, int32_t offset, float scale) {
       STATIC_ASSERT(D::base + D::length <= size, delay_memory_full);
       T w = DataType<format>::Compress(accumulator_);
       if (offset == -1) {
@@ -177,25 +147,22 @@ class FxEngine {
       }
       accumulator_ *= scale;
     }
-    
-    template<typename D>
-    inline void Write(D& d, float scale) {
+
+    template <typename D> inline void Write(D &d, float scale) {
       Write(d, 0, scale);
     }
 
-    template<typename D>
-    inline void WriteAllPass(D& d, int32_t offset, float scale) {
+    template <typename D>
+    inline void WriteAllPass(D &d, int32_t offset, float scale) {
       Write(d, offset, scale);
       accumulator_ += previous_read_;
     }
-    
-    template<typename D>
-    inline void WriteAllPass(D& d, float scale) {
+
+    template <typename D> inline void WriteAllPass(D &d, float scale) {
       WriteAllPass(d, 0, scale);
     }
-    
-    template<typename D>
-    inline void Read(D& d, int32_t offset, float scale) {
+
+    template <typename D> inline void Read(D &d, int32_t offset, float scale) {
       STATIC_ASSERT(D::base + D::length <= size, delay_memory_full);
       T r;
       if (offset == -1) {
@@ -207,24 +174,23 @@ class FxEngine {
       previous_read_ = r_f;
       accumulator_ += r_f * scale;
     }
-    
-    template<typename D>
-    inline void Read(D& d, float scale) {
+
+    template <typename D> inline void Read(D &d, float scale) {
       Read(d, 0, scale);
     }
-    
-    inline void Lp(float& state, float coefficient) {
+
+    inline void Lp(float &state, float coefficient) {
       state += coefficient * (accumulator_ - state);
       accumulator_ = state;
     }
 
-    inline void Hp(float& state, float coefficient) {
+    inline void Hp(float &state, float coefficient) {
       state += coefficient * (accumulator_ - state);
       accumulator_ -= state;
     }
-    
-    template<typename D>
-    inline void Interpolate(D& d, float offset, float scale) {
+
+    template <typename D>
+    inline void Interpolate(D &d, float offset, float scale) {
       STATIC_ASSERT(D::base + D::length <= size, delay_memory_full);
       MAKE_INTEGRAL_FRACTIONAL(offset);
       float a = DataType<format>::Decompress(
@@ -235,10 +201,10 @@ class FxEngine {
       previous_read_ = x;
       accumulator_ += x * scale;
     }
-    
-    template<typename D>
-    inline void Interpolate(
-        D& d, float offset, LFOIndex index, float amplitude, float scale) {
+
+    template <typename D>
+    inline void Interpolate(D &d, float offset, LFOIndex index, float amplitude,
+                            float scale) {
       STATIC_ASSERT(D::base + D::length <= size, delay_memory_full);
       offset += amplitude * lfo_value_[index];
       MAKE_INTEGRAL_FRACTIONAL(offset);
@@ -250,22 +216,23 @@ class FxEngine {
       previous_read_ = x;
       accumulator_ += x * scale;
     }
-    
-   private:
+
+  private:
     float accumulator_;
     float previous_read_;
     float lfo_value_[2];
-    T* buffer_;
+    T *buffer_;
     int32_t write_ptr_;
 
     DISALLOW_COPY_AND_ASSIGN(Context);
   };
-  
+
   inline void SetLFOFrequency(LFOIndex index, float frequency) {
-    lfo_[index].template Init<stmlib::COSINE_OSCILLATOR_APPROXIMATE>(frequency * 32.0f);
+    lfo_[index].template Init<stmlib::COSINE_OSCILLATOR_APPROXIMATE>(frequency *
+                                                                     32.0f);
   }
-  
-  inline void Start(Context* c) {
+
+  inline void Start(Context *c) {
     --write_ptr_;
     if (write_ptr_ < 0) {
       write_ptr_ += size;
@@ -282,19 +249,17 @@ class FxEngine {
       c->lfo_value_[1] = lfo_[1].value();
     }
   }
-  
- private:
-  enum {
-    MASK = size - 1
-  };
-  
+
+private:
+  enum { MASK = size - 1 };
+
   int32_t write_ptr_;
-  T* buffer_;
+  T *buffer_;
   stmlib::CosineOscillator lfo_[2];
-  
+
   DISALLOW_COPY_AND_ASSIGN(FxEngine);
 };
 
-}  // namespace plaits
+} // namespace plaits
 
-#endif  // PLAITS_DSP_FX_FX_ENGINE_H_
+#endif // PLAITS_DSP_FX_FX_ENGINE_H_
